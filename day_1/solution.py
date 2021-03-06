49d56f5ebc531ebcf9fb3d0fdc4dc3c77b20d56a
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-06 08:20:26 +0300

    Update solution.py

--- day_1/solution.py
+++ day_1/solution.py
@@ -1,12 +1,15 @@
+# solution.py
+
 from itertools import combinations
 from functools import reduce
 
-numbers = []
+# create numbers = []
+numbers = [] 
 with open("input.txt", 'r') as file:
     for line in file:
         numbers.append(int(line))
 
-
+# create def find_and_multiply(numbers, size):
 def find_and_multiply(numbers, size):
     for comb in combinations(numbers, size):
         if sum(comb) == 2020:
